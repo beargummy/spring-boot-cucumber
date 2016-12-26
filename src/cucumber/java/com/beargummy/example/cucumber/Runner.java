877eb616ba3d3a36cc30a3056e195cf4a7edc543
Daniil Kudryavtsev <kudryavtsev.d.e@gmail.com>
2016-12-26 14:01:36 +0300

    Move tests to test directory.

--- src/cucumber/java/com/beargummy/example/cucumber/Runner.java
+++ src/cucumber/java/com/beargummy/example/cucumber/Runner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/cucumber/resources")
+@CucumberOptions(features = "src/test/resources")
 public class Runner {
 
 }
